ae208a733446917cece45b3af08e9bfa19be5979
Tomy4986 <tomas.gomezbaiardi@gmail.com>
2025-10-07 10:51:48 -0300

    js de [perfil

--- JS/perfil.js
+++ JS/perfil.js
@@ -5,11 +5,39 @@ const goles = document.getElementById('goles');
 const palos = document.getElementById('Palos');
 const ganados = document.getElementById('partidosGanados');
 
-let editando = false;
+let editandoStats = false;
+let editandoInfoPersonal = false;
 
 function editarStats() {
-    if (!editando) {
+    if (!editandoStats) {
         partidos.innerHTML = '<strong>Partidos Jugados:</strong> <input type="number" id="inputPartidos" value="125">';
+        goles.innerHTML = '<strong>Goles:</strong> <input type="number" id="inputGoles" value="13">';
+        palos.innerHTML = '<strong>Palos:</strong> <input type="number" id="inputPalos" value="37">';
+        ganados.innerHTML = '<strong>Partidos Ganados:</strong> <input type="number" id="inputGanados" value="78">';
+        btnEditar.textContent = 'Guardar';
+        editandoStats = true;
+    } else {
+        const inputPartidos = document.getElementById('inputPartidos').value;
+        const inputGoles = document.getElementById('inputGoles').value;
+        const inputPalos = document.getElementById('inputPalos').value;
+        const inputGanados = document.getElementById('inputGanados').value;
+        partidos.innerHTML = `<strong>Partidos Jugados:</strong> ${inputPartidos}`;
+        goles.innerHTML = `<strong>Goles:</strong> ${inputGoles}`;
+        palos.innerHTML = `<strong>Palos:</strong> ${inputPalos}`;
+        ganados.innerHTML = `<strong>Partidos Ganados:</strong> ${inputGanados}`;
+        btnEditar.textContent = 'Editar';
+        editandoStats = false;
+}}  
+
+function editarInfoPersonal() {
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const telefono = document.getElementById('telefono');
+    const email = document.getElementById('email');
+    const edad = document.getElementById('edad');
+    const ciudad = document.getElementById('ciudad');
+    if (!editandoInfoPersonal) {
+        nombre.innerHTML = '<strong>Partidos Jugados:</strong> <input type="number" id="inputPartidos" value="125">';
         goles.innerHTML = '<strong>Goles:</strong> <input type="number" id="inputGoles" value="13">';
         palos.innerHTML = '<strong>Palos:</strong> <input type="number" id="inputPalos" value="37">';
         ganados.innerHTML = '<strong>Partidos Ganados:</strong> <input type="number" id="inputGanados" value="78">';
@@ -27,5 +55,4 @@ function editarStats() {
         btnEditar.textContent = 'Editar';
         editando = false;
 }}  
-
 btnEditar.addEventListener('click', editarStats);
