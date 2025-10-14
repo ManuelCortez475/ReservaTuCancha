eebd743f29d30a1527840826e08326b7a74c4f8e
Manuel Etchegaray Cortez <metchegaraycortez@udesa.edu.ar>
2025-10-13 21:47:39 -0300

    Unirse js funcionando

--- JS/unirse.js
+++ JS/unirse.js
@@ -1,15 +1,36 @@
-const btnUnirse = document.getElementById('btn-unirse');
+const btnUnirse = document.querySelectorAll('.btn-unirse');
+const CONTRASENA_CORRECTA = 'reserva123'; 
+
 function agregarJugador (event){
     const fila = event.target.closest('tr');
-    const cantidad = fila.querySelector('.cantJugadores');
-    const valor = parseInt(cantidad.innerText);
-    let cantidadJugadores = valor;
-    const unirse = getElementById('btn-unirse');
+    const estado = fila.querySelector('.Estado');
+    const estado_actual = estado.innerText.trim();
+    const cantidadElement = fila.querySelector('.cantJugadores');
+    const tamaño_cancha = cantidadElement.innerText;
+    const [cantidad_inicial, maximo] = tamaño_cancha.split('/');
+    let cantidad = parseInt(cantidad_inicial);
+    const max = parseInt(maximo);
+    
+    if (estado_actual === 'Privada'){
+        const contrasena_ingresada = prompt("Esta cancha es privada. Por favor, ingresa la contraseña:");
 
-    if (0 <= cantidadJugadores <= valor){
-    valor ++;
-    cantidad.innerText = `${valor}/22`;
+        // Si el usuario cancela (null) o la contraseña es incorrecta,
+        if (contrasena_ingresada !== CONTRASENA_CORRECTA) {
+            alert("Contraseña incorrecta o acción cancelada. No puedes unirte.");
+            return;
+        }
+    }
+    if (cantidad < max){
+        cantidad++;
+        cantidadElement.innerText = `${cantidad}/${max}`;
+        
+        if (cantidad === max) {
+            event.target.innerText = 'Cancha Llena';
+            event.target.disabled = true; 
+        }
     }
 }
 
-btnUnirse.addEventListener('click', agregarJugador);
+btnUnirse.forEach(button => {
+    button.addEventListener('click', agregarJugador);
+});
